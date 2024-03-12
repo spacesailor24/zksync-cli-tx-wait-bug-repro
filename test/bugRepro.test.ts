c87aaf4e2035bcebc86c535144c08eabd6edc946
Wyatt Barnes <me@wyatt.email>
2024-03-12 09:24:34 -1000

    Init

---
+++ test/bugRepro.test.ts
@@ -0,0 +1,31 @@
+import { getWallet, LOCAL_RICH_WALLETS } from '../deploy/utils';
+import { parseEther } from "ethers";
+
+describe("BugRepro", function () {
+    const wallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey);
+
+    it("Should transfer 1 ETH", async function() {
+        const receiver = '0xAf0C73483AeD5C2029aB1dF32F3D6B75bA479088';
+        console.log(await wallet.provider.getBalance(receiver));
+    
+        const tx = await wallet.sendTransaction({ value: parseEther('1'), to: receiver });
+        await tx.wait();
+    
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+        console.log(await wallet.provider.getBalance(receiver));
+      });
+});
